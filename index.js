d3304fee6c87f0b79908462058e2927f73cb3752
Christopher Babb <chris@appsoftdev.com>
2022-06-08 20:00:40 -0700

    commit tree ef084f5d244ae48f4bedb1df4470b9ef13a9bd97

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Code your solution here
+function findMatching(arrayInput, toFind){
+    let theNames = []
+    for(const names of arrayInput){
+        if(names.toLowerCase() === toFind.toLowerCase()){
+            theNames.push(names)
+        }
+    }
+    return theNames
+}
+
+function fuzzyMatch(arrayInput, toFind) {
+    let theNames = []   
+    for(const names of arrayInput){
+        let findMatch = ""
+        for(let i=0;i<toFind.length;++i){
+            findMatch += names[i]
+        }
+        if(findMatch === toFind){
+            theNames.push(names)
+        }
+    }
+    return theNames;
+}
+
+function matchName(arrayInput, theName) {
+    let newArray = []
+    for(const theDrivers of arrayInput){
+        console.log(theDrivers)
+        if(theDrivers.name === theName){
+            newArray.push(theDrivers)
+        }
+    }
+    return newArray
+}
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+];
